297d933825f22411406a496bf99b43186c80725e
Daniel Gallik <gallikdaniel@gmail.com>
2016-03-13 18:42:03 +0100

    Mapbuilder skeleton

---
+++ Meteogy.Engine/MapBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Device.Location;
+
+namespace Meteogy.Engine
+{
+    public class MapBuilder
+    {
+        private double[,] map;
+        private GeoCoordinate[] corners;
+
+        public double[,] GetMap()
+        {
+            return map;
+        }
+
+        public MapBuilder(int width, int height, GeoCoordinate leftBot, GeoCoordinate rightTop)
+        {
+            // vytvori inicializaciu mapy
+        }
+
+        public void Insert(GeoCoordinate coordinate, double value)
+        {
+            // vlozi hodnotu do prislusneho policka
+        }
+    }
+}
---
+++ Meteogy.Tests/MapBuilderTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Device.Location;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Meteogy.Engine;
+
+namespace Meteogy.Tests
+{
+    [TestClass]
+    public class MapBuilderTests
+    {
+        [TestMethod]
+        public void TestConstructor1()
+        {
+            GeoCoordinate leftBot = new GeoCoordinate(0, 0);
+            GeoCoordinate rightTop = new GeoCoordinate(1, 1);
+            MapBuilder bulder = new MapBuilder(3, 3, leftBot, rightTop);
+            double[,] actual = bulder.GetMap();
+
+            double[,] expected = new double[3, 3] 
+            { 
+                { 0, 0, 0 }, 
+                { 0, 0, 0 }, 
+                { 0, 0, 0 }
+            };
+
+            Assert.AreEqual(expected.Length, actual.Length);
+            for (var i = 0; i < expected.GetLength(0); i++)
+            {
+                for (var j = 0; j < expected.GetLength(1); j++)
+                {
+                    Assert.AreEqual(expected[i, j], actual[i, j]);
+                }
+            }
+        }
+    }
+}
