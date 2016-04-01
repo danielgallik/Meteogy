50f7110a6b599192fa52d7ee7262d32ad4a46a24
katuskaa <kfabianova11@gmail.com>
2016-04-01 22:14:55 +0200

    MapBuilder

--- Meteogy.Engine/MapBuilder.cs
+++ Meteogy.Engine/MapBuilder.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Device.Location;
+using System.Collections;
 
 namespace Meteogy.Engine
 {
@@ -11,6 +12,12 @@ namespace Meteogy.Engine
     {
         private double[,] map;
         private GeoCoordinate[] corners;
+        private Dictionary<Tuple<int, int>, List<double>> points = new Dictionary<Tuple<int, int>, List<double>>();
+
+
+        private int MIN_WIDTH = 1;
+        private int MIN_HEIGHT = 1;
+
 
         public double[,] GetMap()
         {
@@ -19,12 +26,62 @@ namespace Meteogy.Engine
 
         public MapBuilder(int width, int height, GeoCoordinate leftBot, GeoCoordinate rightTop)
         {
-            // vytvori inicializaciu mapy
+            width = (width < MIN_WIDTH) ? MIN_WIDTH : width;
+            height = (width < MIN_HEIGHT) ? MIN_HEIGHT : height;
+            
+            map = new double[width, height];
+            
+            corners = new GeoCoordinate[2];
+            corners[0] = leftBot;
+            corners[1] = rightTop;
         }
 
-        public void Insert(GeoCoordinate coordinate, double value)
+        public bool Insert(GeoCoordinate coordinate, double value)
         {
-            // vlozi hodnotu do prislusneho policka
+            if (coordinate.Latitude < corners[0].Latitude ||
+                coordinate.Latitude > corners[1].Latitude ||
+                coordinate.Longitude < corners[0].Longitude ||
+                coordinate.Longitude > corners[1].Longitude)
+            {
+                return false;
+            }
+            
+            double deltaX = corners[1].Latitude - corners[0].Latitude;
+            double deltaY= corners[1].Longitude - corners[0].Longitude;
+
+            double startX = coordinate.Latitude - corners[0].Latitude;
+            double startY = coordinate.Longitude - corners[0].Longitude;
+
+            deltaX += deltaX / map.GetLength(1);
+            deltaY += deltaY / map.GetLength(0);
+
+            double stepX = deltaX / map.GetLength(1);
+            double stepY = deltaY / map.GetLength(0);
+
+            int positionX = (int)Math.Round((startX / stepX), MidpointRounding.AwayFromZero);
+            int positionY = (int)Math.Round((map.GetLength(0) - (startY / stepY)) - 1, MidpointRounding.AwayFromZero);
+            
+            Tuple<int, int> key = new Tuple<int, int>(positionY, positionX);
+            
+            if (!points.ContainsKey(key))
+            {
+                points.Add(key, new List<double>());
+            }
+
+            points[key].Add(value);
+
+            foreach (KeyValuePair<Tuple<int, int>, List<double>> point in points)
+            {
+                Tuple<int, int> position = point.Key;
+                List<double> values = point.Value;
+                int countOfValues = values.Count;
+                double average = Math.Round(point.Value.Take(countOfValues).Sum() / countOfValues, 2, MidpointRounding.AwayFromZero);
+
+                map[position.Item1, position.Item2] = average;
+            }
+
+            return true;
         }
     }
 }
+
